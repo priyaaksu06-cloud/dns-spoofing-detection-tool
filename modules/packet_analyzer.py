5c763e9b70fbcacbc70aad2b6d1cd3d9904cdf76
suchithra <suchithraa44@gmail.com>
2026-05-31 09:36:47 +0530

    completed integration module

--- modules/packet_analyzer.py
+++ modules/packet_analyzer.py
@@ -1,4 +1,6 @@
 from scapy.all import DNSRR
+from modules.db_manager import get_trusted_ip, save_dns_record
+from modules.alert_system import generate_alert
 
 
 def analyze_packet(packet):
@@ -16,6 +18,18 @@ def analyze_packet(packet):
             print("\n----------------------")
             print(f"Domain: {domain}")
             print(f"Response: {response}")
+
+            trusted_ip = get_trusted_ip(domain)
+
+            if trusted_ip is None:
+                save_dns_record(domain, str(response))
+                print("New trusted DNS record saved.")
+
+            elif trusted_ip == str(response):
+                print("SAFE DNS RESPONSE")
+
+            else:
+                generate_alert(domain, trusted_ip, str(response))
 
         except Exception as e:
             print(f"Error: {e}")
